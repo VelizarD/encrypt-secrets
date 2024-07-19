0879927be80f260d0b214b80d6e4667806592b46
Velizar Demirev <velizardemirev@gmail.com>
2024-07-19 15:31:57 +0200

    feat: use build-in cipher module to encrypt secrets

--- dist/encrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BINARY_ENCRYPTED_SECRET=$(echo -n "$1" | openssl enc -aes-256-cbc -pbkdf2 -salt -k "$2")
-echo ::set-output name=encrypted-secret::$BINARY_ENCRYPTED_SECRET
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+const crypto = require('crypto');
 
 async function run() {
     try{
         const secrets = core.getInput('secrets');
         const token = core.getInput('github-token');
 
-        await exec.exec(`${__dirname}/encrypt.sh ${secrets} ${token}`);
+        const key = Buffer.from(token.slice(0, 32), 'utf8');
+        const iv = Buffer.from(token.slice(0, 16), 'utf8');
+        const cipher = crypto.createCipheriv('aes256', key, iv);
+
+        const encryptedSecrets = cipher.update(secrets, 'utf8', 'utf8');
+        core.setOutput('encrypted-secret', encryptedSecrets);
     } catch (error) {
         core.setFailed(error.message);
     }
